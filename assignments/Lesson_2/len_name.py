33b0c79cbbabf8e21c14da18a668430a6e4a9457
HawksSpawn <67832524+HawksSpawn@users.noreply.github.com>
2020-07-20 12:44:12 +0200

    Add solution to length name problem [Lesson_2]

---
+++ assignments/Lesson_2/len_name.py
@@ -0,0 +1,8 @@
+given_name = "William "
+middle_names = "Bradley "
+family_name = "Pitt"
+driving_license_character_limit = 28
+
+name_length = len(given_name) + len(middle_names) + len(family_name)
+
+print(name_length <= driving_license_character_limit)
