a4389ab9de4a697f1164795761980ac3aec163f4
HawksSpawn <67832524+HawksSpawn@users.noreply.github.com>
2020-07-20 13:05:22 +0200

    Update string_methods.py

--- assignments/Lesson_2/string_methods.py
+++ assignments/Lesson_2/string_methods.py
@@ -1,11 +1,11 @@
 verse = ("If you can keep your head when all about you\n"
-        "Are losing theirs and blaming it on you,\n"
-        "If you can trust yourself when all men doubt you,\n"
-        "But make allowance for their doubting too;\n"
-        "If you can wait and not be tired by waiting,\n"
-        "Or being lied about, don’t deal in lies,\n"
-        "Or being hated, don’t give way to hating,\n"
-        "And yet don’t look too good, nor talk too wise:")
+         "Are losing theirs and blaming it on you,\n"
+         "If you can trust yourself when all men doubt you,\n"
+         "But make allowance for their doubting too;\n"
+         "If you can wait and not be tired by waiting,\n"
+         "Or being lied about, don’t deal in lies,\n"
+         "Or being hated, don’t give way to hating,\n"
+         "And yet don’t look too good, nor talk too wise:")
 
 print(verse)
 print()
