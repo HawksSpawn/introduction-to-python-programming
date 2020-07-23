cd5ec712b151ea31170005d405bd407a6e418cc4
HawksSpawn <67832524+HawksSpawn@users.noreply.github.com>
2020-07-23 16:04:14 +0200

    Update password_generator.py

--- assignments/Lesson_6/password_generator.py
+++ assignments/Lesson_6/password_generator.py
@@ -6,10 +6,10 @@ word_list = []
 with open(word_file, 'r') as words:
 	for line in words:
         # remove white space and make everything lowercase
-		word = line.strip().lower()
+        word = line.strip().lower()
         # don't include words that are too long or too short
-		if 3 < len(word) < 8:
-			word_list.append(word)
+        if 3 < len(word) < 8:
+            word_list.append(word)
 
 def generate_password():
     """Return a string consisting of three random words concatenated together without spaces."""
