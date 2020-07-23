032f50c94cf738e845b51ce13b680e7e251f3915
HawksSpawn <67832524+HawksSpawn@users.noreply.github.com>
2020-07-23 12:45:52 +0200

    Add solution to password generator problem [Lesson_6]

---
+++ assignments/Lesson_6/password_generator.py
@@ -0,0 +1,19 @@
+from random import choice
+
+word_file = "words.txt"
+word_list = []
+
+with open(word_file,'r') as words:
+	for line in words:
+		# remove white space and make everything lowercase
+		word = line.strip().lower()
+		# don't include words that are too long or too short
+		if 3 < len(word) < 8:
+			word_list.append(word)
+
+def generate_password():
+    """Return a string consisting of three random words concatenated together without spaces."""
+    password = [choice(word_list) for i in range(3)]
+    return ("").join(password)
+
+print(generate_password())
